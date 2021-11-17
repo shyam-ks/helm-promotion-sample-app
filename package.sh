c7ad6ec035f78b140208366b1e255fdea682f53c
S <shyam.s@bakerhughes.com>
2021-11-17 19:05:31 +0530

    app11

--- package.sh
+++ package.sh
@@ -10,7 +10,7 @@ python3 ./utils/update-chart-version.py ./chart/$1/Chart.yaml 'version' $2
 
 mkdir -p package
 cd package
-helm package ../chart/*
+helm package ../chart/$1
 helm repo index .
 
 cd ..
